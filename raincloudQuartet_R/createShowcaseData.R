3fce9521cc290f29b2b7117cf372f3fafc72adac
Vincent Ott <123652963+vincentott@users.noreply.github.com>
2024-05-03 15:04:32 +0200

    design3x1

--- raincloudQuartet_R/createShowcaseData.R
+++ raincloudQuartet_R/createShowcaseData.R
@@ -30,12 +30,6 @@ design3x1 <- data.frame(
     rep("levelC", sampleSizePerGroup)
   ),
 
-  dependentVariable = c(
-    rnorm(mean = 0.49,  sd = 0.09, n = sampleSizePerGroup),
-    rnorm(mean = 0.46, sd = 0.10, n = sampleSizePerGroup),
-    rnorm(mean = 0.67,  sd = 0.11, n = sampleSizePerGroup)
-  ),
-
   covariate = c(  # Higher in the first two levels than in levelC
     round(rnorm(mean = 34, sd = 6, n = sampleSizePerGroup)),
     round(rnorm(mean = 34, sd = 6, n = sampleSizePerGroup)),
@@ -47,16 +41,56 @@ design3x1 <- data.frame(
 # Check that covariate is above 0
 design3x1 %>% filter(covariate < 0) %>% nrow()
 
+# Add dependentVariable
+set.seed(1)
+dvLevelA <- rnorm(mean = 0.50,  sd = 0.10, n = sampleSizePerGroup)
+dvLevelA <- sort(dvLevelA, decreasing = TRUE)
 
-# Visualize
+design3x1$dependentVariable <- rep(dvLevelA, 3)
+
+# Visualize interim result
 ggplot(design3x1, aes(factorA, dependentVariable, fill = factorA)) +
-  geom_rain(alpha = .5, id.long.var = "id") +
+  geom_rain(alpha = .5, cov = "covariate", id.long.var = "id") +
   theme_classic() +
   scale_fill_brewer(palette = 'Dark2') +
   guides(fill = 'none') +
   scale_color_viridis_c(option =  "A")
 
 
-# Create some odd observations, that can be detected with subjectLines
+# IDs 6 to 20 wont change across the three levels, but the rest will
+set.seed(1)
+dvLevelB <- dvLevelA + c(
+  rnorm(mean = -0.01, sd = 0.05, 5),
+  rep(0, 15),
+  rnorm(mean = -0.01, sd = 0.05, 30)
+)
+
+set.seed(1)
+dvLevelC <- dvLevelA + c(
+  rnorm(mean = 0.10, sd = 0.05, 5),
+  rep(0, 15),
+  rnorm(mean = 0.11, sd = 0.05, 30)
+)
+
+design3x1$dependentVariable <- c(dvLevelA, dvLevelB, dvLevelC)
+
+ggplot(design3x1, aes(factorA, dependentVariable, fill = factorA)) +
+  geom_rain(
+    alpha = .5,
+    id.long.var = "id",
+    cov = "covariate"
+  ) +
+  theme_classic() +
+  scale_fill_brewer(palette = 'Dark2') +
+  guides(fill = 'none') +
+  scale_color_viridis_c(option =  "A")
+
+
+# write.csv(design3x1, "./showcaseData/design3x1.csv")  # Commented out just for safety
+
+
+
+# 2x2 Design ----
+
 
 
