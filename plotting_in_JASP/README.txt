bbdaacd887713d3381a492afbef401f9d71a4ed5
Vincent Ott <123652963+vincentott@users.noreply.github.com>
2025-06-29 20:48:50 +0200

    ensure accessible colors

--- plotting_in_JASP/README.txt
+++ plotting_in_JASP/README.txt
@@ -5,3 +5,5 @@ that shows the identical means and confidence intervals next to each other.
 design3x1 also covers two more plots that are not in the article but featured
 in the YouTube tutorial. One shows means connected by lines,
 the other shows subject trajectories and custom cloud orientations.
+
+accessibleColorsCheck.R checks that all colors are accessible for colorblind people.
---
+++ plotting_in_JASP/accessibleColorsCheck.R
@@ -0,0 +1,13 @@
+library("colorblindcheck")
+
+# Raincloud quartet
+palette_check(c("#56B4E9", "#CC79A7", "#009E73", "#E69F00"), plot = TRUE)  # Okabe & Ito (2008)
+
+# Figure 4: One factorial design with inferential content
+palette_check(c("#CB79A7", "#98C160", "#000000"), plot = TRUE)
+
+# Covariate
+palette_check(c("#FDE725", "#5EC962", "#21918C", "#3B528B", "#440154"), plot = TRUE)
+
+# Twofactorial design
+palette_check(c("#FE979C", "#73A8C4"), plot = TRUE)
